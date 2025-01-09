39de46e17cbd20ecd1c2234934685c1993329bc7
Lenin Acevedo <lenin.acevedo999@hotmail.com>
2025-01-09 11:42:30 -0600

    add mainly routes and controllers for the app

--- App/proyect.controller.js
+++ App/proyect.controller.js
@@ -1,5 +1,5 @@
 const { ApiResponse } = require("./interfaces.model.js");
-const { BigQuery } = require('@google-cloud/bigquery');
+
 
 //#region General
 
@@ -9,26 +9,55 @@ const ConnectionTest = (req, res) => {
 
 const login = async (req, res) => {
     try {
-        const bigquery = new BigQuery();
-        const query = `SELECT * FROM WREdemo.ods_login WHERE email = '${req.body.email}' AND password = '${req.body.password}'`;
-        const options = {
-            query: query,
-            location: 'US',
-        };
-        const [job] = await bigquery.createQueryJob(options);
-        const [rows] = await job.getQueryResults();
-        if (rows.length <= 0)
+        const result = await pool.request()
+        .input('Usuario', sql.VarChar, usuario)
+        .input('Token', sql.VarChar, token)
+        .input('IdEmpresa', sql.Int, idEmpresa)
+        .execute('spr_pp_getLogin');
+        if (result.recordset.length <= 0)
             return res.status(401).json({ Exito: "false", mensaje: "Invalid email/password", Data: {} })
         else
-            return ApiResponse(rows, res)
+            return ApiResponse(result, res)
     } catch (e) {
         console.log(e)
         return ApiResponse(null, res, 'Error al obtener el dashboard')
     }
 }
 
+
+const authValidator = async (req, res, next) => {
+    try {
+      const usuario = req.headers['usuario'];
+      const token = req.headers['authorization']?.replace('Bearer ', '');
+      const idEmpresa = req.headers['idempresa'];
+  
+      if (!usuario || !token || !idEmpresa) {
+        return res.status(400).json({ mensaje: 'Faltan datos de autenticación' });
+      }
+  
+      const pool = await sql.connect(dbConfig);
+  
+      const result = await pool.request()
+        .input('Usuario', sql.VarChar, usuario)
+        .input('Token', sql.VarChar, token)
+        .input('IdEmpresa', sql.Int, idEmpresa)
+        .execute('sp_ValidarSesion');
+  
+      if (result.returnValue !== 0) {
+        return res.status(401).json({ mensaje: 'User or token are not valid' });
+      }
+  
+    //   req.usuario = result.recordset[0];
+  
+      next();
+    } catch (err) {
+      console.error('Error en la validación:', err);
+      res.status(500).json({ mensaje: 'Error interno en la validación' });
+    }
+}
+
 //#endregion
 
 module.exports = {
-    ConnectionTest, login,
+    ConnectionTest, login, authValidator
 }
--- App/proyect.routes.js
+++ App/proyect.routes.js
@@ -1,14 +1,15 @@
 const { Router } = require("express");
 const {
-    ConnectionTest, login,
+    ConnectionTest, login, authValidator
 } = require("./proyect.controller.js");
 
 const router = Router();
-const urlBase = '/api/proyect/v1';
+const urlBase = '/api/pp/v1';
 
 router.get(`${urlBase}/ConnectionTest`, ConnectionTest);
 
 router.post(`${urlBase}/login`, login);
+router.post(`${urlBase}/activity`, authValidator, login);
 
 
 module.exports = router;
