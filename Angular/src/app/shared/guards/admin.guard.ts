2dcf59a680f2213a95d98dedbef6f00a61f659d3
Lenin Acevedo <lenin.acevedo999@hotmail.com>
2025-01-09 11:41:42 -0600

    manage guard for admin permissions

--- Angular/src/app/shared/guards/admin.guard.ts
+++ Angular/src/app/shared/guards/admin.guard.ts
@@ -1,5 +1,20 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { ContextService } from 'src/app/services/services/context.service';
+import { environment } from 'src/environments/environment';
 
 export const adminGuard: CanActivateFn = (route, state) => {
-  return true;
+  const _context = inject(ContextService);
+    const router = inject(Router);
+  
+    if (!environment.production)
+      return true;
+    else {
+      if (_context.isAuth() && [1,2].includes(_context.theRol())) 
+        return true;
+      else {
+        router.navigate(['/login'])
+        return false;
+      }
+    }
 };
