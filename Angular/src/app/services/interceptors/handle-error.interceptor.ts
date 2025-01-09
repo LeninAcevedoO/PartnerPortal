6c959d1138eb9a3835471d11294fb994e2a1e581
Lenin Acevedo <lenin.acevedo999@hotmail.com>
2025-01-09 11:40:51 -0600

    add headers to every request in the interceptor

--- Angular/src/app/services/interceptors/handle-error.interceptor.ts
+++ Angular/src/app/services/interceptors/handle-error.interceptor.ts
@@ -3,7 +3,8 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
@@ -12,21 +13,37 @@ import { ToastrService } from 'ngx-toastr';
 export class HandleErrorInterceptor implements HttpInterceptor {
 
   
-  constructor(private toastr: ToastrService) { }
+  constructor(private toastr: ToastrService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(request)
-      .pipe(catchError(err => {
-        if (err.status == 0)
-          this.toastr.error('Verifica tu conexión a internet', 'Error de conexión');
-        if (err.status == 404)
+    // Recuperar los valores de sessionStorage
+    const usuario = sessionStorage.getItem('usuario') || '';
+    const token = sessionStorage.getItem('sessionToken') || '';
+    const idEmpresa = sessionStorage.getItem('idEmpresa') || '';
+
+    // Clonar la solicitud y agregar encabezados personalizados
+    const modifiedRequest = request.clone({
+      setHeaders: {
+        Usuario: usuario,
+        Authorization: `Bearer ${token}`, // Incluye el token si se requiere en formato Bearer
+        IdEmpresa: idEmpresa,
+      },
+    });
+
+    return next.handle(modifiedRequest).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 0) {
+          this.toastr.error('Verify your internet connection', 'Bad conection');
+        } else if (err.status === 404) {
+          this.toastr.error('There\'s no information found', 'Error 404');
           return [];
-        if (err.status != 0) {
-          const mensaje = err.error.mensaje ? err.error.mensaje : err.error.message
-          this.toastr.error(mensaje, 'Error ' + err.status);
+        } else {
+          const mensaje = err.error?.mensaje || err.error?.message || 'Something went wrong, try again later';
+          this.toastr.error(mensaje, `Error ${err.status}`);
         }
 
         return throwError(() => new Error(err.message));
-      }));
+      })
+    );
   }
 }
