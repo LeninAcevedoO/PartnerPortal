550fd03d482805550136e37e7255683caf1ead88
Jaxel562 <erick.juarez@resultant.com>
2025-01-15 14:51:17 -0600

    Adjust of timer

--- Angular/src/app/shared/components/auto-logout/auto-logout.component.ts
+++ Angular/src/app/shared/components/auto-logout/auto-logout.component.ts
@@ -1,14 +1,37 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-auto-logout',
   templateUrl: './auto-logout.component.html',
   styleUrls: ['./auto-logout.component.scss'],
 })
-export class AutoLogoutComponent {
+export class AutoLogoutComponent implements OnInit, OnDestroy {
   @Output() continueSession = new EventEmitter<void>();
+
+  timeLeft: number = 60; 
+  private intervalId: any; 
+
+  ngOnInit(): void {
+    this.startCountdown();
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 
   stayActive(): void {
     this.continueSession.emit();
+  }
+
+  private startCountdown(): void {
+    this.intervalId = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        clearInterval(this.intervalId); 
+      }
+    }, 1000);
   }
 }
