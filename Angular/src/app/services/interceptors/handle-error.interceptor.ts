c04712cf68d9279507345d4fecf11109bd41cc72
Lenin Acevedo <lenin.acevedo999@hotmail.com>
2025-02-10 14:56:25 -0600

    Mod interceptor adding ip client, headers and cyphered send and get information

--- Angular/src/app/services/interceptors/handle-error.interceptor.ts
+++ Angular/src/app/services/interceptors/handle-error.interceptor.ts
@@ -1,55 +1,91 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, timeout } from 'rxjs/operators';
+import { Observable, throwError, from } from 'rxjs';
+import { catchError, tap, timeout, switchMap } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
 import { UtilsService } from 'src/app/shared/utils/utils.service';
+import * as platform from 'platform';
+import { Router } from '@angular/router';
+import { ContextService } from '../services/context.service';
 
 @Injectable()
 export class HandleErrorInterceptor implements HttpInterceptor {
   private readonly REQUEST_TIMEOUT = 10000;
   
-  constructor(private toastr: ToastrService,
-    private _utilsSvc: UtilsService
+  constructor(
+    private toastr: ToastrService, 
+    private _utilsSvc: UtilsService,
+    private router: Router,
+    private _context: ContextService
   ) {}
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // Recuperar los valores de sessionStorage
-    const usuario = sessionStorage.getItem('usuario') || '';
-    const token = sessionStorage.getItem('sessionToken') || '';
-    const idEmpresa = sessionStorage.getItem('idEmpresa') || '';
-    let modifiedRequest = request.clone({
-      setHeaders: {
-        Usuario: usuario,
-        Authorization: `Bearer ${token}`,
-        IdEmpresa: idEmpresa,
-      },
-    });
-
-    if (request.body) {
-      console.log(request.body)
-      const encryptedBody = this._utilsSvc.encryptAES(JSON.stringify(request.body));
-      modifiedRequest = modifiedRequest.clone({ body: { "data": encryptedBody} });
+  private async getClientIp(): Promise<string> {
+    try {
+      const response = await fetch('https://api64.ipify.org?format=json');
+      const data = await response.json();
+      return data.ip;
+    } catch (error) {
+      console.error('Error obteniendo la IP:', error);
+      return 'unknown';
     }
+  }
 
-    return next.handle(modifiedRequest).pipe(
-      timeout(this.REQUEST_TIMEOUT), // 🚀 Timeout de 10 segundos
-      catchError((err: any) => {
-        if (err.name === 'TimeoutError') {
-          this.toastr.error('The request took too long, please try again later.', 'Request Timeout');
-          return throwError(() => new Error('Request Timeout'));
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const currentRoute = this.router.url || 'unknown';
+
+    const usuario = sessionStorage.getItem('mat__resultant__pp__id') || '';
+    const token = sessionStorage.getItem('mat__resultant__pp__tkn') || '';
+    const idEmpresa = sessionStorage.getItem('mat__resultant__pp__company_id') || '';
+
+    const deviceInfo = platform.description || 'unknown';
+
+    return from(this.getClientIp()).pipe(
+      switchMap(ip => {
+        let modifiedRequest = request.clone({
+          setHeaders: {
+            'X-User': usuario,
+            Authorization: `Bearer ${token}`,
+            'X-Company': idEmpresa,
+            'X-Client-IP': ip,
+            'X-Device-Info': deviceInfo,
+            'X-Current-Route': currentRoute
+          },
+        });
+
+        if (request.body) {
+          console.log(request.body);
+          const encryptedBody = this._utilsSvc.encryptAES(JSON.stringify(request.body));
+          modifiedRequest = modifiedRequest.clone({ body: { "data": encryptedBody } });
         }
 
-        if (err.status === 0) {
-          this.toastr.error('Verify your internet connection', 'Bad connection');
-        } else if (err.status === 404) {
-          this.toastr.error('There\'s no information found', 'Error 404');
-        } else {
-          const mensaje = err.error?.mensaje || err.error?.message || 'Something went wrong, try again later';
-          this.toastr.error(mensaje, `Error ${err.status}`);
-        }
+        return next.handle(modifiedRequest).pipe(
+          tap((data: any) => {
+            if (data.type !== 0) {
+              data.body.data = JSON.parse(this._utilsSvc.decryptAES(data.body.data));
+              // console.log(data.body.data)
+            }
+          }),
+          timeout(this.REQUEST_TIMEOUT),
+          catchError((err: any) => {
+            if (err.name === 'TimeoutError') {
+              this.toastr.error('The request took too long, please try again later.', 'Request Timeout');
+              return throwError(() => new Error('Request Timeout'));
+            }
+            if (err.status === 0) {
+              this.toastr.error('Verify your internet connection', 'Bad connection');
+            } else if (err.status === 401) {
+              this.toastr.warning('You\'re not authorizated', 'Error 401');
+              this._context.logout();
+            } else if (err.status === 404) {
+              this.toastr.error('There\'s no information found', 'Error 404');
+            } else {
+              const mensaje = err.error?.mensaje || err.error?.message || 'Something went wrong, try again later';
+              this.toastr.error(mensaje, `Error ${err.status}`);
+            }
 
-        return throwError(() => new Error(err.message));
+            return throwError(() => new Error(err.message));
+          })
+        );
       })
     );
   }
