80b835b5231b684296468b1a76b79085b659ca9d
Lenin Acevedo <lenin.acevedo999@hotmail.com>
2025-02-12 02:32:09 -0600

    change enviroments

--- Angular/src/environments/environment.prod.ts
+++ Angular/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  gcloud: 'https://wre-poc-service-5eiikqaheq-uc.a.run.app',
-  debugPC: 'http://localhost:4200',
+  gcloud: "http://34.16.17.59:8001/",
+  debugPC: "http://34.16.17.59:8001/",
+  aesKey: "1f8b8d4c72b42f08f4ad5a9e2b2a3c12",
+  aesIv: "8a1b2c3d4e5f6g7h",
 };
