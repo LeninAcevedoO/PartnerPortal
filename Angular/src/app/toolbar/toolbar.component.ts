a58226fc5c13b95ff7d5a37fea01522216c84b02
Lenin Acevedo <lenin.acevedo999@hotmail.com>
2025-03-05 12:54:11 -0600

    fix navigate toolbar route

--- Angular/src/app/toolbar/toolbar.component.ts
+++ Angular/src/app/toolbar/toolbar.component.ts
@@ -1,35 +1,87 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { MatTreeFlatDataSource, MatTreeFlattener, MatTreeModule } from '@angular/material/tree';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { ContextService } from 'src/app/services/services/context.service';
-import { FlatTreeControl } from '@angular/cdk/tree';  
-import { MatDialog } from '@angular/material/dialog'; 
-import { MainService } from 'src/app/services/services/main.service';
-import { NewManagementCommentComponent } from '../pages/new-management-comment/new-management-comment.component';
+import { Component, ChangeDetectionStrategy } from "@angular/core";
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener,
+  MatTreeModule,
+} from "@angular/material/tree";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { ContextService } from "src/app/services/services/context.service";
+import { FlatTreeControl } from "@angular/cdk/tree";
+import { MatDialog } from "@angular/material/dialog";
+import { MainService } from "src/app/services/services/main.service";
+import { NewManagementCommentComponent } from "../pages/new-management-comment/new-management-comment.component";
 
 const TREE_DATA: any[] = [
   {
-    name: 'Home', icon: 'home', permissions: [1, 2, 3, 4],
+    name: "Home",
+    icon: "home",
+    permissions: [1, 2, 3, 4],
+    onClick: "/",
     children: [
-      { name: 'Advertisements', onClick: '/home', icon: 'campaign', permissions: [1, 2, 3, 4] }, 
-      { name: 'About us', onClick: '/about-us', icon: 'info', permissions: [1, 2, 3, 4] }, 
+      {
+        name: "Advertisements",
+        onClick: "/home",
+        icon: "campaign",
+        permissions: [1, 2, 3, 4],
+      },
+      {
+        name: "About us",
+        onClick: "/about-us",
+        icon: "info",
+        permissions: [1, 2, 3, 4],
+      },
     ],
   },
-  { name: 'Favorites', onClick: '/favorites', icon: 'star', permissions: [1, 2, 3, 4] },
-  { name: 'AI Assistant', permissions: [1, 2, 3], onClick: '/login', icon: 'support_agent' },
-  { name: 'Verticals', onClick: '/verticals', icon: 'vertical_distribute', permissions: [1, 2, 3, 4]},
+  {
+    name: "Favorites",
+    onClick: "/favorites",
+    icon: "star",
+    permissions: [1, 2, 3, 4],
+  },
+  {
+    name: "AI Assistant",
+    permissions: [1, 2, 3],
+    onClick: "/login",
+    icon: "support_agent",
+  },
+  {
+    name: "Verticals",
+    onClick: "/verticals",
+    icon: "vertical_distribute",
+    permissions: [1, 2, 3, 4],
+  },
   // { name: 'Services' },
-  { name: 'Manager Comments', permissions: [1, 2, 3, 4], onClick: 'openModalNewComment', icon: 'contact_mail' },
-  { name: 'Dashboard', permissions: [1, 2], onClick: '/login', icon: 'dashboard' },
-  { name: 'Settings', permissions: [1], onClick: '/settings', icon: 'settings' },
-  { name: 'Logout', onClick: 'Logout', icon: 'logout', permissions: [1, 2, 3, 4] },
+  {
+    name: "Manager Comments",
+    permissions: [1, 2, 3, 4],
+    onClick: "openModalNewComment",
+    icon: "contact_mail",
+  },
+  {
+    name: "Dashboard",
+    permissions: [1, 2],
+    onClick: "/login",
+    icon: "dashboard",
+  },
+  {
+    name: "Settings",
+    permissions: [1],
+    onClick: "/settings",
+    icon: "settings",
+  },
+  {
+    name: "Logout",
+    onClick: "Logout",
+    icon: "logout",
+    permissions: [1, 2, 3, 4],
+  },
 ];
 
 @Component({
-  selector: 'app-toolbar',
-  templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss'],
+  selector: "app-toolbar",
+  templateUrl: "./toolbar.component.html",
+  styleUrls: ["./toolbar.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ToolbarComponent {
@@ -44,20 +96,20 @@ export class ToolbarComponent {
       level: level,
       icon: node.icon,
       onClick: node.onClick,
-      permissions: node.permissions
+      permissions: node.permissions,
     };
   };
 
   treeControl = new FlatTreeControl<any>(
-    node => node.level,
-    node => node.expandable,
+    (node) => node.level,
+    (node) => node.expandable
   );
 
   treeFlattener = new MatTreeFlattener(
     this._transformer,
-    node => node.level,
-    node => node.expandable,
-    node => node.children,
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => node.children
   );
 
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
@@ -83,7 +135,7 @@ export class ToolbarComponent {
   }
 
   // filterMenuItems() {
-  //   this.dataSource.data = TREE_DATA.filter(item => item.permissions.includes(this.userRole));  
+  //   this.dataSource.data = TREE_DATA.filter(item => item.permissions.includes(this.userRole));
   //   //this.shouldShowMenuItem(item.name));
   // }
 
@@ -103,20 +155,22 @@ export class ToolbarComponent {
   openModalProfile() {}
 
   openModalNewComment() {
-    this.dialog.open(NewManagementCommentComponent, {
-      panelClass: 'post-dialog-container',
-    }).afterClosed().subscribe(result => {
-      if (result) {
-        this.toastr.success('Comment added successfully', 'Success');
-      }
-    });
+    this.dialog
+      .open(NewManagementCommentComponent, {
+        panelClass: "post-dialog-container",
+      })
+      .afterClosed()
+      .subscribe((result) => {
+        if (result) {
+          this.toastr.success("Comment added successfully", "Success");
+        }
+      });
   }
-  
 
   logout() {
     this._context.logout();
-    this.toastr.info('The session has been closed', 'Bye');
-    this.router.navigate(['/login']);
+    this.toastr.info("The session has been closed", "Bye");
+    this.router.navigate(["/login"]);
   }
 
   actionClick(node: any) {
@@ -130,26 +184,27 @@ export class ToolbarComponent {
     //   Services: '/services',
     //   Verticals: '/verticals'
     // };
-
-    switch(node.onClick) {
-      case 'openModalNewComment':
-        this.openModalNewComment();
-        break;
-      case 'Logout':
-        this.logout();
-        break;
-      default:
-        this.router.navigate([node.route]);
-        break;
+    if (node.onClick.length > 2) {
+      switch (node.onClick) {
+        case "openModalNewComment":
+          this.openModalNewComment();
+          break;
+        case "Logout":
+          this.logout();
+          break;
+        default:
+          this.router.navigate([node.onClick]);
+          this.opened = false;
+          break;
+      }
     }
-
     // if (node.name === 'Manager Comments') {
-    //   this.openModalNewComment(); 
+    //   this.openModalNewComment();
     //   return;
     // }
 
     // if (node.name === 'Logout') {
-    //   this.logout(); 
+    //   this.logout();
     //   return;
     // }
 
