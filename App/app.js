7d5bd2287e608eddb0626ebf5f29fc613282c63d
Lenin Acevedo <lenin.acevedo999@hotmail.com>
2025-03-15 16:11:14 -0600

    remove inicesary imports

--- App/app.js
+++ App/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const proyectRoutes = require('./proyect.routes.js');
 const cors = require('cors');
-const path = require("path");
 
 const app = express();
 app.use(cors());
