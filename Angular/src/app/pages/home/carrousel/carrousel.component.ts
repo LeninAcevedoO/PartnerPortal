99c7306c25e22c23fd2a8fbcf5b293ccd8dbeed5
Lenin Acevedo <lenin.acevedo999@hotmail.com>
2025-03-20 21:41:42 -0600

    add carrousel

--- Angular/src/app/pages/home/carrousel/carrousel.component.ts
+++ Angular/src/app/pages/home/carrousel/carrousel.component.ts
@@ -1,34 +1,52 @@
-import { Component } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-import { MainService } from 'src/app/services/services/main.service';
-import { Resultado } from 'src/app/shared/models/general.model';
+import { ChangeDetectorRef, Component } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
+import { MainService } from "src/app/services/services/main.service";
+import { Resultado } from "src/app/shared/models/general.model";
 
 @Component({
-  selector: 'app-carrousel',
-  templateUrl: './carrousel.component.html',
-  styleUrls: ['./carrousel.component.scss']
+  selector: "app-carrousel",
+  templateUrl: "./carrousel.component.html",
+  styleUrls: ["./carrousel.component.scss"],
 })
 export class CarrouselComponent {
-  posts: any[] = [];
+  demos: any[] = [];
   myFavorites: string[] = [];
   currentIndex: number = 0;
 
-  constructor(private _service: MainService,
-    private toastr: ToastrService
+  constructor(
+    private _service: MainService,
+    private toastr: ToastrService,
+    private cdRef: ChangeDetectorRef
   ) {}
 
-  async setFavorite(post: any) {
-    (await this._service.setFavorite(post.favorite_id)).subscribe((resp: Resultado) => {
-      if (resp.success == 'true') 
-        this.toastr.success('');
+  async ngOnInit() {
+    await this.getAllDemos();
+  }
+
+  async getAllDemos() {
+    (await this._service.getDemosByVertical(0)).subscribe((resp: Resultado) => {
+      if (resp.success == "true") {
+        this.demos = resp.data;
+        this.cdRef.detectChanges();
+      } else {
+        this.toastr.error("Error al obtener los demos");
+      }
     });
   }
 
+  async setFavorite(post: any) {
+    (await this._service.setFavorite(post.favorite_id)).subscribe(
+      (resp: Resultado) => {
+        if (resp.success == "true") this.toastr.success("");
+      }
+    );
+  }
+
   nextSlide() {
-    if (this.currentIndex < this.posts.length - 1) {
+    if (this.currentIndex < Math.ceil(this.demos.length / 5) - 1) {
       this.currentIndex++;
     } else {
-      this.currentIndex = 0; // Reinicia al inicio
+      this.currentIndex = 0;
     }
   }
 
@@ -36,8 +54,7 @@ export class CarrouselComponent {
     if (this.currentIndex > 0) {
       this.currentIndex--;
     } else {
-      this.currentIndex = this.posts.length - 1; // Vuelve al final
+      this.currentIndex = this.demos.length - 1;
     }
   }
-
 }
