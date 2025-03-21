9938379a2978ee8e8b091ffb2ba481c8b9afe5e8
Lenin Acevedo <lenin.acevedo999@hotmail.com>
2025-03-20 21:42:10 -0600

    add carrrousel at home

--- Angular/src/app/pages/home/home.component.ts
+++ Angular/src/app/pages/home/home.component.ts
@@ -13,18 +13,4 @@ export class HomeComponent {
   constructor(private toastr: ToastrService, private utilsService: UtilsService, private http: HttpClient) {}
 
   comment: string = 'Hello, this is a test';
-
-  onSubmit() {
-    console.log('Original Comment:', this.comment);
-
-  
-    const encryptedComment = this.utilsService.encryptAES(this.comment);
-    console.log('Encrypted Comment:', encryptedComment);
-
-  
-    this.http.post<any>('http://localhost:3000/api/encrypt-decrypt', { encryptedComment })
-      .subscribe(response => {
-        console.log('Response from API (re-encrypted comment):', response.decryptedComment);
-      });
-  }
 }
