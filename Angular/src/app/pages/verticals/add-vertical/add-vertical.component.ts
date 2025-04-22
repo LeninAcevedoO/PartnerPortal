1d2315237fbb6ed232ee39dd2b9fbb6d01209189
Lenin Acevedo <lenin.acevedo999@hotmail.com>
2025-04-22 07:20:11 -0600

    add isMainCarousel to form and

--- Angular/src/app/pages/verticals/add-vertical/add-vertical.component.ts
+++ Angular/src/app/pages/verticals/add-vertical/add-vertical.component.ts
@@ -1,44 +1,53 @@
-import { Component, Inject } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
-import * as moment from 'moment';
-import { ToastrService } from 'ngx-toastr';
-import { MainService } from 'src/app/services/services/main.service';
-import { MultimediaViewerComponent } from 'src/app/shared/components/multimedia-viewer/multimedia-viewer.component';
-import { Resultado } from 'src/app/shared/models/general.model';
-import { getMimeTypeFromBase64, validarNumeros } from 'src/app/shared/utils/utils.functions';
+import { Component, Inject } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatDialog,
+} from "@angular/material/dialog";
+import * as moment from "moment";
+import { ToastrService } from "ngx-toastr";
+import { MainService } from "src/app/services/services/main.service";
+import { MultimediaViewerComponent } from "src/app/shared/components/multimedia-viewer/multimedia-viewer.component";
+import { Resultado } from "src/app/shared/models/general.model";
+import {
+  convertBase64ToWebP,
+  getMimeTypeFromBase64,
+  getMultimediaType,
+  validarNumeros,
+} from "src/app/shared/utils/utils.functions";
 
 @Component({
-  selector: 'app-add-vertical',
-  templateUrl: './add-vertical.component.html',
-  styleUrls: ['./add-vertical.component.scss']
+  selector: "app-add-vertical",
+  templateUrl: "./add-vertical.component.html",
+  styleUrls: ["./add-vertical.component.scss"],
 })
 export class AddVerticalComponent {
-
   titulo = "Add new demo";
   formDemo = new FormGroup({
     demo_id: new FormControl<number>(0),
-    demo_name: new FormControl<string>('', Validators.required),
-    description: new FormControl<string>('', Validators.required),
-    information: new FormControl<string>('',  [Validators.required]),
-    release_date: new FormControl<string>('', Validators.required),
-    vertical_id: new FormControl<string>('', Validators.required),
-    miniature: new FormControl<string>(''),
-    fileName: new FormControl<string>(''),
-    multimedia_type_id: new FormControl<string>(''),
+    demo_name: new FormControl<string>("", Validators.required),
+    description: new FormControl<string>("", Validators.required),
+    information: new FormControl<string>("", [Validators.required]),
+    release_date: new FormControl<string>("", Validators.required),
+    vertical_id: new FormControl<string>("", Validators.required),
+    miniature: new FormControl<any>(""),
+    fileName: new FormControl<string>(""),
+    isMainCarusel: new FormControl<boolean>(false),
+    multimedia_type_id: new FormControl<string>(""),
   });
   today = new Date();
   cats: any = {
     verticals: [
-      {id: 1, description: 'Education'},
-      {id: 2, description: 'Human/Health Services'},
-      {id: 3, description: 'Workforce Development '},
-      {id: 4, description: 'DOC & Criminal Justice'},
-      {id: 5, description: 'Public Sector Solutions'},
-      {id: 7, description: 'Private Sector Solutions'},
-      {id: 6, description: 'Others'},
-    ]
-  }
+      { id: 1, description: "Education" },
+      { id: 2, description: "Human/Health Services" },
+      { id: 3, description: "Workforce Development " },
+      { id: 4, description: "DOC & Criminal Justice" },
+      { id: 5, description: "Public Sector Solutions" },
+      { id: 7, description: "Private Sector Solutions" },
+      { id: 6, description: "Others" },
+    ],
+  };
 
   constructor(
     private _service: MainService,
@@ -50,14 +59,15 @@ export class AddVerticalComponent {
 
   ngOnInit() {
     if (this.data?.demo_id) {
+      console.log(this.data);
       this.formDemo.patchValue(this.data);
       this.titulo = "Edit demo";
     }
   }
 
   AddEditVertical() {
-    if(this.formDemo.invalid) {
-      this.toastr.warning('The form is not valid, try again', 'Form not valid');
+    if (this.formDemo.invalid) {
+      this.toastr.warning("The form is not valid, try again", "Form not valid");
       return;
     }
     if (this.formDemo.value.demo_id) this.EditVertical();
@@ -67,28 +77,40 @@ export class AddVerticalComponent {
   EditVertical = async () => {
     let demo = { ...this.formDemo.value };
     (await this._service.updateDemo(demo)).subscribe((resp: Resultado) => {
-      if(resp.success == 'true') {
-        this.toastr.success('The demo was updated successfully', 'Success');
+      if (resp.success == "true") {
+        this.toastr.success("The demo was updated successfully", "Success");
         this.onNoClick(true);
       } else {
-        this.toastr.error(resp.message, 'Error');
+        this.toastr.error(resp.message, "Error");
       }
     });
   };
 
   AddVertical = async () => {
-    let demo = { ...this.formDemo.value,
-      release_date: moment(this.formDemo.value.release_date).format('YYYY-MM-DD'),
-      multimedia_type_id: 1,
-      mimeType: getMimeTypeFromBase64(this.formDemo.value.miniature || ''),
+    const mimeT = getMimeTypeFromBase64(this.formDemo.value.miniature || "");
+    if (mimeT?.includes("image")) {
+      this.formDemo.patchValue({
+        miniature: convertBase64ToWebP(
+          this.formDemo.value.miniature || "",
+          0.8
+        ),
+      });
+    }
+    let demo = {
+      ...this.formDemo.value,
+      release_date: moment(this.formDemo.value.release_date).format(
+        "YYYY-MM-DD"
+      ),
+      multimedia_type_id: getMultimediaType(mimeT || ""),
+      mimeType: mimeT,
       fileName: this.formDemo.value.demo_name,
-     };
+    };
     (await this._service.setDemo(demo)).subscribe((resp: Resultado) => {
-      if(resp.success == 'true') {
-        this.toastr.success('The demo was updated successfully', 'Success');
+      if (resp.success == "true") {
+        this.toastr.success("The demo was updated successfully", "Success");
         this.onNoClick(true);
       } else {
-        this.toastr.error(resp.message, 'Error');
+        this.toastr.error(resp.message, "Error");
       }
     });
   };
@@ -106,7 +128,7 @@ export class AddVerticalComponent {
     if (file) {
       const reader = new FileReader();
       reader.readAsDataURL(file);
-      reader.onload = () => { 
+      reader.onload = () => {
         this.formDemo.patchValue({
           miniature: String(reader.result),
           fileName: file.name,
@@ -117,12 +139,12 @@ export class AddVerticalComponent {
 
   openVisualizer() {
     const info = {
-      mimeType: 'img',
-      b64: this.formDemo.value.miniature
-    }
+      mimeType: "img",
+      b64: this.formDemo.value.miniature,
+    };
     this.dialog.open(MultimediaViewerComponent, {
-      panelClass: 'post-dialog-container',
-      data: info
-    })
+      panelClass: "post-dialog-container",
+      data: info,
+    });
   }
 }
